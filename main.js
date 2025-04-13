7197d7c0ffab5624aae85c438fe1147cdcd49454
rafaellolufas <166717631+rafaellolufas@users.noreply.github.com>
2025-04-13 17:41:01 +0000

    alterado

--- main.js
+++ main.js
@@ -15,7 +15,12 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-08-24T00:00:00");
+const tempoObjetivo2 = new Date("2026-05-25T00:00:00");
+const tempoObjetivo3 = new Date("2026-05-16T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-16T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
@@ -27,24 +32,22 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [0, 0, 0, 0];
+        return [0,0,0,0];
     }
 }
-
 function atualizaCronometro() {
     for (let i = 0; i < contadores.length; i++) {
         document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
         document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
-    }
+}
 }
 
 function comecaCronometro() {
